0daec33a79d9afe393e21827b41ee62b50e3b6f0
Faiza Mazhar <phaizah85@gmail.com>
2022-01-10 11:16:13 +0000

    Initial commit

---
+++ app/src/main/java/com/example/layoutgeneration/LayoutGeneration.kt
@@ -0,0 +1,41 @@
+package com.example.layoutgeneration
+
+class LayoutGeneration {
+    fun generateLayout(items: List<Int>): List<PromoWithSpan> {
+        val promoList = mutableListOf<PromoWithSpan>()
+
+        promoList.add(0, PromoWithSpan(
+            HierarchicalCollectionPromoType.LargePromo,
+            SpanType.Full
+            )
+        )
+
+        val alone = (items.size - 1) % 2
+        val pairs = (items.size - 1) / 2
+
+
+        for (index in 1 .. items.size) {
+            promoList.add(createGridPromo())
+        }
+
+        return promoList
+    }
+}
+
+fun createGridPromo() = PromoWithSpan(HierarchicalCollectionPromoType.GridPromo, SpanType.Half)
+
+class PromoWithSpan(val promoType: HierarchicalCollectionPromoType, val spanType: SpanType)
+
+enum class HierarchicalCollectionPromoType {
+    LargePromo,
+    GridPromo,
+    HorizontalPromo,
+    HorizontalTextOnlyPromo,
+}
+
+enum class SpanType(val span: Int) {
+    Full(12),
+    Half(6),
+    OneThird(4),
+    Quarter(3),
+}
---
+++ app/src/test/java/com/example/layoutgeneration/LayoutGenerationTest.kt
@@ -0,0 +1,48 @@
+package com.example.layoutgeneration
+
+import org.junit.Assert
+import org.junit.Test
+
+
+internal class LayoutGenerationTest {
+
+    private val layoutGeneration = LayoutGeneration()
+    @Test
+    fun `first promo is always a large promo with span Full`() {
+        val list = layoutGeneration.generateLayout(listOf(1,2,3))
+        val expected = PromoWithSpan(HierarchicalCollectionPromoType.LargePromo, SpanType.Full)
+        Assert.assertEquals(expected.promoType, list.first().promoType)
+        Assert.assertEquals(expected.spanType, list.first().spanType)
+    }
+
+    @Test
+    fun `for three promo, the promo 2 and 3 are marked as grid promos with span Half`() {
+        val list = layoutGeneration.generateLayout(listOf(1,2,3))
+
+        val expected = listOf(
+            PromoWithSpan(HierarchicalCollectionPromoType.LargePromo, SpanType.Full),
+            PromoWithSpan(HierarchicalCollectionPromoType.GridPromo, SpanType.Half),
+            PromoWithSpan(HierarchicalCollectionPromoType.GridPromo, SpanType.Half),
+        )
+        Assert.assertEquals(expected[1].promoType, list[1].promoType)
+        Assert.assertEquals(expected[1].spanType, list[1].spanType)
+
+        Assert.assertEquals(expected[2].promoType, list[2].promoType)
+        Assert.assertEquals(expected[2].spanType, list[2].spanType)
+    }
+
+    @Test
+    fun `for four promos, the promo 2,3,4 are marked horizontal`() {
+        val list = layoutGeneration.generateLayout(listOf(1,2,3,4))
+
+        val expected = listOf(
+            PromoWithSpan(HierarchicalCollectionPromoType.LargePromo, SpanType.Full),
+            PromoWithSpan(HierarchicalCollectionPromoType.HorizontalPromo, SpanType.Full),
+            PromoWithSpan(HierarchicalCollectionPromoType.HorizontalPromo, SpanType.Full),
+            PromoWithSpan(HierarchicalCollectionPromoType.HorizontalPromo, SpanType.Full),
+        )
+
+        Assert.assertEquals(expected[1].promoType, list[1].promoType)
+        Assert.assertEquals(expected[1].spanType, list[1].spanType)
+    }
+}
